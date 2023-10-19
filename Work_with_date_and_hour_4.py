81f5618766e0a035db1daf05b8fa479a47e1fc22
xcalius44 <xcalius25@gmail.com>
2023-10-19 17:20:04 +0300

    update

--- Work_with_date_and_hour_4.py
+++ Work_with_date_and_hour_4.py
@@ -9,9 +9,9 @@ days = [31 ,59 ,90 ,120 ,151 ,181 ,212 ,243 ,273 ,304 ,334 ,365]
 
 print("Current date and time:", current_time) 
 print("Current year:", today_date.year) 
-print("Current month:", today_date.month) 
-print("week number of the year:", today_date.day)
+print("Current month:", today_date.strftime("%B")) 
+print("week number of the year:", today_date.strftime("%U") )
 print("Current day:", today_date.weekday() + 1)
-print("day of year:", days[today_date.month - 1] + today_date.day) 
+print("day of year:", today_date.strftime("%j")) 
 print("Current day:", today_date.day)
-print("Day of Week:", calendar.day_name[today_date.weekday()])
+print("Day of Week:", today_date.strftime("%A"))
