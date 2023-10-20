f6041545cb1ad575000cddb79946d8e78ff0661b
xcalius44 <xcalius25@gmail.com>
2023-10-20 18:00:09 +0300

    UPDATE

--- Work_with_date_and_hour_7.py
+++ Work_with_date_and_hour_7.py
@@ -8,10 +8,18 @@ def spaniel(n , days):
             if days[day - 1] == 1:
                 day_weeks.append(day_number)
                 day_number = 0
-            number += 1
+                number += 1
         else:
            day_number += 1 
+           if day == n - 1:
+               day_weeks.append(day_number)
     print(len(day_weeks),(day_weeks))  
     return(len(day_weeks),(day_weeks))
 print(spaniel(8, [1 ,1 ,1 ,1 ,1 ,1 ,1 ,1]) == (1, [8]))
+print(spaniel(2, [1 ,1]) == (1, [8]))
+print(spaniel(2, [0 ,0]) == (1, [8]))
+print(spaniel(3, [1 ,0 ,0]) == (1, [8]))
+print(spaniel(5, [1 ,0 ,1 ,1 ,1]) == (1, [8]))
+print(spaniel(8, [1 ,0 ,1 ,0 ,0 ,1 ,1 ,1]) == (1, [8]))
+
     
