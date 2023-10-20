66d5e6fe40eec85139fce664ad14bf52c90efce1
xcalius44 <xcalius25@gmail.com>
2023-10-20 17:38:22 +0300

    UPDATE

--- Work_with_date_and_hour_7.py
+++ Work_with_date_and_hour_7.py
@@ -2,20 +2,16 @@ def spaniel(n , days):
     day_weeks = []
     day_number = 0
     number = 0
+    pas = 0
     for day in range(0, n):
         if days[day] == 0:
-            day_weeks.append(day_number)
-            day_number = 0
+            if days[day - 1] == 1:
+                day_weeks.append(day_number)
+                day_number = 0
             number += 1
         else:
            day_number += 1 
-    if number == 0:
-        day_weeks.append(day_number)
-        day_number = 0
-    if number == n:
-        return('0','[]')
-    elif number != n: 
-        print(len(day_weeks),(day_weeks))  
-        return(len(day_weeks),(day_weeks))
-print(spaniel(2, [0 ,0]) == (0, []))
+    print(len(day_weeks),(day_weeks))  
+    return(len(day_weeks),(day_weeks))
+print(spaniel(8, [1 ,1 ,1 ,1 ,1 ,1 ,1 ,1]) == (1, [8]))
     
