a131fbf1e6964d881c18e85cc72edea56d4f65a8
xcalius44 <xcalius25@gmail.com>
2023-10-20 15:23:04 +0300

    UPDATE

--- Work_with_date_and_hour_7.py
+++ Work_with_date_and_hour_7.py
@@ -1,9 +1,7 @@
-def skahguel():
-    n = int(input())
+def spaniel(n , days):
     day_weeks = []
     day_number = 0
     number = 0
-    days =[int(i) for i in input().split()]
     for day in range(0, n):
         if days[day] == 0:
             day_weeks.append(day_number)
@@ -11,11 +9,13 @@ def skahguel():
             number += 1
         else:
            day_number += 1 
-    if number == n or number == 0:
+    if number == 0:
         day_weeks.append(day_number)
         day_number = 0
-        
-    print(len(day_weeks))
-    print(day_weeks)
-skahguel()
+    if number == n:
+        return('0','[]')
+    elif number != n: 
+        print(len(day_weeks),(day_weeks))  
+        return(len(day_weeks),(day_weeks))
+print(spaniel(2, [1 ,1]) == (1, [2]))
     
